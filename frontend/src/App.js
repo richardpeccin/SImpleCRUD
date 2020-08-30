5245a6573eac4b7ddd35b88038d24e8de322219c
RichardPeccin <richard_fpeccin@hotmail.com>
2020-08-30 06:37:09 -0300

    added componet menu.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Header from './components/Header'
+import Menu from './components/Menu'
+import './app.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="menu-content">
+        <Menu />
+        <div className="content"></div>
+      </div>
+      
     </div>
   );
 }
---
+++ frontend/src/components/Menu/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css'
+
+const Menu = () => (
+  <div className="menu">
+    <a href="">Primeiro</a>
+    <a href="">Segundo</a>
+  </div>
+)
+
+export default Menu
